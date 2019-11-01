159e5e077b63c436afae30b19fb007f475892368
RoKang <bykang096@gmail.com>
2019-11-01 12:02:59 +0900

    Update JZRowHeader.swift
    9 to 6 Timetable

--- JZCalendarWeekView/ReusableViews/JZRowHeader.swift
+++ JZCalendarWeekView/ReusableViews/JZRowHeader.swift
@@ -30,10 +30,12 @@ open class JZRowHeader: UICollectionReusableView {
 
     open func setupBasic() {
         // Hide all content when colum header height equals 0
-        self.clipsToBounds = true
+//        self.clipsToBounds = true
         dateFormatter.dateFormat = "HH:mm"
         lblTime.textColor = JZWeekViewColors.rowHeaderTime
-        lblTime.font = UIFont.systemFont(ofSize: 12)
+        
+        lblTime.font = UIFont.systemFont(ofSize: 8)
+        lblTime.textAlignment = .center
     }
 
     public func updateView(date: Date) {
